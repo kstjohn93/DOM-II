e38a0c276c0f3cd1a01579f2e97167779b2239d4
kstjohn93 <50053852+kstjohn93@users.noreply.github.com>
2019-09-09 21:53:40 -0500

    added first event listener- bg green

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+allH2 = document.querySelectorAll('h2');
+// 1
+let h2Click = allH2.forEach(event => {
+    event.addEventListener('click', (event) => {
+        event.target.style.backgroundColor = 'green';
+    })
+});
+
